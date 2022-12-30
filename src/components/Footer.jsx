ea879e6d59056d12bec5ab5a05a94ba3658914e7
Poongothai-R <107067696+Poongothai-R@users.noreply.github.com>
2022-12-30 23:46:38 +0530

    updated and fixed css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import Github from "../assests/contacts/github.png";
+import Linkedin from "../assests/contacts/linkedin.png";
+import '../styles/contact.css';
+export default function Footer(){
+    return(
+<div className="footer">
+    <div className="footer-icon">
+        <a href="https://github.com/Poongothai-R/" target="_blank" rel="noreferrer">
+            <img src={Github} alt="github-logo" /> </a>
+        <a href="https://www.linkedin.com/in/poongothai-rajesh-38320087/" target="_blank" rel="noreferrer">
+            <img src={Linkedin} alt="linkedin-logo" /> </a>
+    </div>
+    <div className='copyright'><span>©  Poongothai Rajesh 2022</span></div>
+</div>
+    );
+}
